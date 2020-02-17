59d9d801e4e2f659b6ea34868bc91f94f79a7a19
Egri Dániel <egri.dani.r@gmail.com>
2020-02-17 09:34:21 +0100

    first steps

--- CsvReading/CsvReading/Program.cs
+++ CsvReading/CsvReading/Program.cs
@@ -7,7 +7,30 @@ namespace CsvReading
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+
+
+        }       
+    }
+
+    class auto
+    {
+        string _brand;
+        string _name;
+        int _year;
+        int _distance;
+        string _fuel;
+        int _price;
+
+        public auto(string brand, string name, int year, int distance, string fuel, int price)
+        {
+            _brand = brand;
+            _name = name;
+            _year = year;
+            _distance = distance;
+            _fuel = fuel;
+            _price = price;
         }
+
     }
 }
